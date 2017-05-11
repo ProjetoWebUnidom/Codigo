6f911ab222d5cb8aecb0fff2ab85037fcf46c8c3
mateus <mateus-vinicius19@live.com>
2017-05-11 17:08:28 -0300

    novo tipo de usuario

--- view/php/permissao.php
+++ view/php/permissao.php
@@ -2,16 +2,16 @@
 
 function permissaoAcesso($acesso){
   if($acesso==4){
-    $_SESSION["ID_TipoUsuario"]  = 4;
+    $_SESSION["ID_TipoUsuario"]  = 2;
   }elseif($acesso ==3){
-    $_SESSION["ID_TipoUsuario"]  = 3;
+    $_SESSION["ID_TipoUsuario"]  = 1;
   }
   return $_SESSION["ID_TipoUsuario"];
 }
 
 function perfil(){
-  if(isset($_SESSION["ID_TipoUsuario"]) && $_SESSION["ID_TipoUsuario"]==4){
-      return include "../../includes/headerFuncionario.html";  
+  if(isset($_SESSION["ID_TipoUsuario"]) && $_SESSION["ID_TipoUsuario"]==2){
+      return include "../../includes/headerFuncionario.html";
   }else{
       return include "../../includes/headerAdm.html";
     }
