97615bc041b45d8dd58ac3c77f00fc5a2134572f
mateus <mateus-vinicius19@live.com>
2017-05-11 17:12:53 -0300

    alteracao no usuario

--- view/php/banco-funcionario.php
+++ view/php/banco-funcionario.php
@@ -1,9 +1,9 @@
 <?php
     function buscarUsuario($conn,$usuario,$senha){
       $stmt = $conn->prepare("SELECT LOGIN_Funcionario,ID_TipoUsuario FROM funcionario WHERE LOGIN_Funcionario = ?
-                                AND SENHA_Funcionario = ? AND ID_TipoUsuario>=2");
+                                AND SENHA_Funcionario = ?");
       $stmt->bind_param("ss",$usuario,$senha);
       $stmt->execute();
       $resultado = $stmt->get_result();
-      return $resultado;
+      return $resultado->fetch_assoc();
     }
