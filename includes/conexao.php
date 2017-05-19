9ebf64b74a40809fe7d129747fe0bb21c32ace35
Victor.Fernando <victorfernando.sis@gmail.com>
2017-05-19 15:02:40 -0300

    Update conexao.php

--- includes/conexao.php
+++ includes/conexao.php
@@ -1,6 +1,5 @@
 <?php
-$conn = new mysqli("localhost:3306", "root","", "karina");
-//$conn = new mysqli("localhost:3306", "root","pk2437439", "karina");
+$conn = new mysqli("localhost:3306", "root","pk2437439", "karina");
     if ($conn->connect_error) {
         die("Connection failed: " . $conn1->connect_error);
     }
